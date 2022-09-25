28670c163b6a774bc754c60192d7ba6a3dd8482d
VladimirMinenko <minvladserg@gmail.com>
2022-09-25 13:29:15 +0300

    Binding to Setter

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -7,5 +7,15 @@ import { Component, Input } from '@angular/core';
 })
 export class ChildComponent {
   @Input() userName: string = '';
-  @Input() userAge: number = 0;
+  _userAge: number = 0;
+
+  @Input()
+  set userAge(age: number) {
+    if (age < 0) this._userAge = 0;
+    else if (age > 100) this._userAge = 100;
+    else this._userAge = age;
+  }
+  get userAge() {
+    return this._userAge;
+  }
 }
