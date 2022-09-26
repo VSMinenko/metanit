a3751a138e1f5f699bf8fa5ab89391f1f72e7ed8
VladimirMinenko <minvladserg@gmail.com>
2022-09-26 22:11:44 +0300

    User interaction, HostListener  HostBinding

--- src/app/bold.directive.ts
+++ src/app/bold.directive.ts
@@ -1,9 +1,22 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 @Directive({
   selector: '[bold]',
+  host: {
+    '(mouseenter)': 'onMouseEnter()',
+    '(mouseleave)': 'onMouseLeave()',
+  },
 })
 export class BoldDirective {
-  constructor(private elementRef: ElementRef) {
-    this.elementRef.nativeElement.style.fontWeight = 'bold';
+  constructor(private element: ElementRef, private renderer: Renderer2) {
+    this.renderer.setStyle(this.element.nativeElement, 'cursor', 'pointer');
+  }
+  onMouseEnter() {
+    this.setFontWeight('bold');
+  }
+  onMouseLeave() {
+    this.setFontWeight('normal');
+  }
+  private setFontWeight(val: string) {
+    this.renderer.setStyle(this.element.nativeElement, 'font-weight', val);
   }
 }
