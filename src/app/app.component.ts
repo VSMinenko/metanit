e4676f75e869d9b674800441da321b4d57b6f6f1
VladimirMinenko <minvladserg@gmail.com>
2022-09-26 22:49:09 +0300

    Structural directives ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'metanit';
-  condition: boolean = true;
-
-  toggle() {
-    this.condition = !this.condition;
-  }
+  items = ['Tom', 'Bob', 'Sam', 'Bill'];
 }
