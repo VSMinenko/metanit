641b7b5ed1951de2c4637571236e34312212815f
VladimirMinenko <minvladserg@gmail.com>
2022-09-26 22:03:12 +0300

    created folder bold.directive.ts and Creating Attribute Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ChildComponent } from './components/child/child.component';
+import { BoldDirective } from './bold.directive';
 
 @NgModule({
-  declarations: [AppComponent, ChildComponent],
+  declarations: [AppComponent, ChildComponent, BoldDirective],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
