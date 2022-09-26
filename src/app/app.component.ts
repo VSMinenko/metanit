a8a491933468ec9f15fa0444b87ca7b3f23822ff
VladimirMinenko <minvladserg@gmail.com>
2022-09-26 21:49:37 +0300

    Changing styles dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'metanit';
+  visibility: boolean = true;
+  // переключаем переменную
+  toggle() {
+    this.visibility = !this.visibility;
+  }
 }
